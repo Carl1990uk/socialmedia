a6a9b70be1deb1de2e9bdaf3bc856d531ea77f89
Carl Clarke <carlclarke1990@me.com>
2022-07-18 14:57:31 +0200

    feat: login system, reset password, login, create

--- src/app/tools/authenticator/authenticator.component.ts
+++ src/app/tools/authenticator/authenticator.component.ts
@@ -15,7 +15,79 @@ export class AuthenticatorComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  onResetClick(resetEmail: HTMLInputElement){
+    let email = resetEmail.value;
+    if(this.isNotEmpty(email)){
+      this.firebasetsAuth.sendPasswordResetEmail(
+        {
+          email: email,
+          onComplete: (err) => {
+            alert(`Reset email sent to ${email} `);
+          }
+        }
+      );
+    }
+  }
+  onLogin(
+    loginEmail:HTMLInputElement,
+    loginPassword: HTMLInputElement
+    ){
+    let email = loginEmail.value;
+    let password = loginPassword.value;
 
+    if(this.isNotEmpty(email) && this.isNotEmpty(password)){
+      this.firebasetsAuth.signInWith(
+        {
+          email: email,
+          password: password,
+          onComplete: (uc) => {
+            alert("Logged In")
+          },
+          onFail: (err) => {
+            alert(err)
+          }
+        }
+      );
+    }
+  }
+  onRegisterClick(
+    registerEmail: HTMLInputElement,
+    registerPassword: HTMLInputElement,
+    registerConfirmPassword: HTMLInputElement
+    ){
+    let email = registerEmail.value;
+    let password = registerPassword.value;
+    let confirmPassword = registerConfirmPassword.value;
+    if(
+      this.isNotEmpty(email)&&
+      this.isNotEmpty(password)&&
+      this.isNotEmpty(confirmPassword)&&
+      this.isAMatch(password, confirmPassword)){
+      this.firebasetsAuth.createAccountWith(
+        {
+          email: email,
+          password: password,
+          onComplete: (uc) => {
+            alert("Account Created");
+            registerEmail.value = "";
+            registerPassword.value = "";
+            registerConfirmPassword.value = "";
+          },
+          onFail: (err) => {
+            alert("Failed to create the account");
+          }
+        }
+        );
+      }
+    }
+
+  isNotEmpty(text: string){
+    return text != null && text.length > 0;
+  }
+
+  isAMatch(text: string, comparedWith: string){
+    return text == comparedWith;
+  }
   onForgotPasswordClick(){
     this.state = AuthenticatorCompState.FORGOT_PASSWORD;
   }
