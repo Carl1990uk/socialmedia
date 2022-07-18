98973c86937b75c36d2677b14f48282578b68dac
Carl Clarke <carlclarke1990@me.com>
2022-07-18 16:06:23 +0200

    feat: email ver, function for: login, reg, forgot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
+import { FirebaseTSAuth } from 'firebasets/firebasetsAuth/firebaseTSAuth';
+import { FirebaseTSApp } from 'firebasets/firebasetsApp/firebaseTSApp';
 import { AuthenticatorComponent } from './tools/authenticator/authenticator.component';
 import { Component } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,8 +11,39 @@ import { MatBottomSheet } from '@angular/material/bottom-sheet';
 })
 export class AppComponent {
   title = 'socialmedia';
-  constructor(private loginSheet: MatBottomSheet){
+  auth = new FirebaseTSAuth();
 
+  constructor(private loginSheet: MatBottomSheet){
+    this.auth.listenToSignInStateChanges(
+      user => {
+        this.auth.checkSignInState(
+          {
+            whenSignedIn: user => {
+              alert();
+
+            },
+            whenSignedOut: user => {
+              alert();
+            },
+            whenSignedInAndEmailNotVerified: user => {
+              user.emailVerified
+            },
+            whenSignedInAndEmailVerified: user => {
+              alert();
+            },
+            whenChanged: user => {
+              alert();
+            }
+          }
+        )
+      }
+    )
+  }
+  onLogoutClick(){
+    this.auth.signOut();
+  }
+  loggedIn(){
+return this.auth.isSignedIn();
   }
   onLoginClick(){
     this.loginSheet.open(AuthenticatorComponent);
